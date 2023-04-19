60658e198cb5fc62f414f4dd7d26b40083f9afe1
Johnson Ojo <johnsonojo89@gmail.com>
2023-04-19 06:57:58 +0100

    cleanup global types

--- src/types/index.t.ts
+++ src/types/index.t.ts
@@ -3,11 +3,6 @@ export {};
 declare global {
   namespace Express {
     interface Request {
-      first_name: string;
-      last_name: string;
-      email: string;
-      password: string;
-      phone_number: string;
       payload: object | any;
     }
   }
