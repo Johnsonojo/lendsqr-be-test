2e50e5ae59757ecfff882409be8d7c013e435436
Johnson Ojo <johnsonojo89@gmail.com>
2023-04-21 10:57:52 +0100

    updated unit tests for wallet feature

--- src/tests/wallet/wallet.spec.ts
+++ src/tests/wallet/wallet.spec.ts
@@ -38,153 +38,205 @@ describe("Wallet Controller", () => {
     req.close();
   });
 
-  describe("Wallet Creation", () => {
-    it("should not create a wallet with empty currency", async () => {
-      const res = await req
-        .post(`${baseWalletRoute}/create`)
-        .set("authorization", `Bearer ${userToken}`)
-        .send(incorrectWalletDetail1);
+  describe("Single Wallet ", () => {
+    describe("Wallet Creation and Funding", () => {
+      let res: ChaiHttp.Response;
 
-      expect(res.status).to.equal(400);
-      expect(res.body).to.be.an("object");
-      expect(res.body.message).to.equal(
-        "Your missed a required fields: currency"
-      );
-      expect(res.body.status).to.equal("failure");
+      it("should allow authenticated user to create a wallet", async () => {
+        res = await req
+          .post(`${baseWalletRoute}/create`)
+          .set("authorization", `Bearer ${userToken}`)
+          .send(completeWalletDetail);
+
+        expect(res.status).to.equal(201);
+        expect(res.body).to.be.an("object");
+        expect(res.body.message).to.equal("Wallet created successfully");
+        expect(res.body.data).to.be.an("object");
+        expect(res.body.data).to.have.property("id");
+        expect(res.body.data).to.have.property("user_id");
+        expect(res.body.data).to.have.property("balance");
+        expect(res.body.data.balance).to.equal("0.00");
+        expect(res.body.data).to.have.property("currency");
+        expect(res.body.data).to.have.property("account_name");
+        expect(res.body.data).to.have.property("account_number");
+      });
+
+      it("should not allow authenticated user to create more than one wallet", async () => {
+        const res2 = await req
+          .post(`${baseWalletRoute}/create`)
+          .set("authorization", `Bearer ${userToken}`)
+          .send(completeWalletDetail);
+
+        expect(res2.status).to.equal(409);
+        expect(res2.body).to.be.an("object");
+        expect(res2.body.message).to.equal("User already has a wallet");
+        expect(res2.body.status).to.equal("failure");
+      });
+
+      it("should allow user to fund a wallet", async () => {
+        const res2 = await req
+          .post(`${baseWalletRoute}/fund/${res.body.data.account_number}`)
+          .set("authorization", `Bearer ${userToken}`)
+          .send(completeFundingDetail);
+
+        expect(res2.status).to.equal(200);
+        expect(res2.body).to.be.an("object");
+        expect(res2.body.message).to.equal("Wallet funded successfully");
+        expect(res2.body.data).to.be.an("object");
+        expect(res2.body.data).to.have.property("id");
+        expect(res2.body.data).to.have.property("user_id");
+        expect(res2.body.data).to.have.property("balance");
+        expect(res2.body.data.balance).to.equal("1000.00");
+        expect(res2.body.data).to.have.property("currency");
+        expect(res2.body.data).to.have.property("account_name");
+        expect(res2.body.data).to.have.property("account_number");
+      });
+
+      it("should not fund a wallet with empty amount", async () => {
+        const res1 = await req
+          .post(`${baseWalletRoute}/fund/${res.body.data.account_number}}`)
+          .set("authorization", `Bearer ${userToken}`)
+          .send(incorrectFundingDetail1);
+
+        expect(res1.status).to.equal(400);
+        expect(res1.body).to.be.an("object");
+        expect(res1.body.message).to.equal(
+          "Your missed a required fields: amount"
+        );
+        expect(res1.body.status).to.equal("failure");
+      });
+
+      it("should not fund a wallet with invalid amount format", async () => {
+        const res2 = await req
+          .post(`${baseWalletRoute}/fund/${res.body.data.account_number}}`)
+          .set("authorization", `Bearer ${userToken}`)
+          .send(incorrectFundingDetail2);
+
+        expect(res2.status).to.equal(400);
+        expect(res2.body).to.be.an("object");
+        expect(res2.body.message).to.equal("Invalid amount format");
+        expect(res2.body.status).to.equal("failure");
+      });
     });
 
-    it("should not create a wallet with invalid currency format", async () => {
-      const res = await req
-        .post(`${baseWalletRoute}/create`)
-        .set("authorization", `Bearer ${userToken}`)
-        .send(incorrectWalletDetail2);
+    describe("Wallet Creation and Funding Issues", () => {
+      it("should not create a wallet with empty currency", async () => {
+        const res = await req
+          .post(`${baseWalletRoute}/create`)
+          .set("authorization", `Bearer ${userToken}`)
+          .send(incorrectWalletDetail1);
 
-      expect(res.status).to.equal(400);
-      expect(res.body).to.be.an("object");
-      expect(res.body.message).to.equal(
-        "The base currency should be either NGN or USD"
-      );
-      expect(res.body.status).to.equal("failure");
-    });
+        expect(res.status).to.equal(400);
+        expect(res.body).to.be.an("object");
+        expect(res.body.message).to.equal(
+          "Your missed a required fields: currency"
+        );
+        expect(res.body.status).to.equal("failure");
+      });
 
-    it("should not allow malformed auth token user to create a wallet", async () => {
-      const res = await req
-        .post(`${baseWalletRoute}/create`)
-        .set("authorization", `Bearer ${malformedToken}`)
-        .send(completeWalletDetail);
+      it("should not create a wallet with invalid currency format", async () => {
+        const res = await req
+          .post(`${baseWalletRoute}/create`)
+          .set("authorization", `Bearer ${userToken}`)
+          .send(incorrectWalletDetail2);
 
-      expect(res.status).to.equal(403);
-      expect(res.body).to.be.an("object");
-      expect(res.body.message).to.equal("Invalid token");
-      expect(res.body.status).to.equal("failure");
-    });
+        expect(res.status).to.equal(400);
+        expect(res.body).to.be.an("object");
+        expect(res.body.message).to.equal(
+          "The base currency should be either NGN or USD"
+        );
+        expect(res.body.status).to.equal("failure");
+      });
 
-    it("should not allow expired auth token user to create a wallet", async () => {
-      const res = await req
-        .post(`${baseWalletRoute}/create`)
-        .set("authorization", `Bearer ${expiredToken}`)
-        .send(completeWalletDetail);
+      it("should not allow malformed auth token user to create a wallet", async () => {
+        const res = await req
+          .post(`${baseWalletRoute}/create`)
+          .set("authorization", `Bearer ${malformedToken}`)
+          .send(completeWalletDetail);
 
-      expect(res.status).to.equal(403);
-      expect(res.body).to.be.an("object");
-      expect(res.body.message).to.equal("jwt expired");
-      expect(res.body.status).to.equal("failure");
-    });
+        expect(res.status).to.equal(403);
+        expect(res.body).to.be.an("object");
+        expect(res.body.message).to.equal("Invalid token");
+        expect(res.body.status).to.equal("failure");
+      });
 
-    it("should not allow unauthorized creation of a wallet", async () => {
-      const res = await req
-        .post(`${baseWalletRoute}/create`)
-        .send(completeWalletDetail);
+      it("should not allow expired auth token user to create a wallet", async () => {
+        const res = await req
+          .post(`${baseWalletRoute}/create`)
+          .set("authorization", `Bearer ${expiredToken}`)
+          .send(completeWalletDetail);
 
-      expect(res.status).to.equal(401);
-      expect(res.body).to.be.an("object");
-      expect(res.body.message).to.equal("Unauthorized action. Access denied");
-    });
+        expect(res.status).to.equal(403);
+        expect(res.body).to.be.an("object");
+        expect(res.body.message).to.equal("jwt expired");
+        expect(res.body.status).to.equal("failure");
+      });
 
-    it("should allow authenticated user to create a wallet", async () => {
-      const res = await req
-        .post(`${baseWalletRoute}/create`)
-        .set("authorization", `Bearer ${userToken}`)
-        .send(completeWalletDetail);
+      it("should not allow unauthorized creation of a wallet", async () => {
+        const res = await req
+          .post(`${baseWalletRoute}/create`)
+          .send(completeWalletDetail);
 
-      expect(res.status).to.equal(201);
-      expect(res.body).to.be.an("object");
-      expect(res.body.message).to.equal("Wallet created successfully");
-      expect(res.body.data).to.be.an("object");
-      expect(res.body.data).to.have.property("id");
-      expect(res.body.data).to.have.property("user_id");
-      expect(res.body.data).to.have.property("balance");
-      expect(res.body.data.balance).to.equal("0.00");
-      expect(res.body.data).to.have.property("currency");
-      expect(res.body.data).to.have.property("account_name");
-      expect(res.body.data).to.have.property("account_number");
+        expect(res.status).to.equal(401);
+        expect(res.body).to.be.an("object");
+        expect(res.body.message).to.equal("Unauthorized action. Access denied");
+      });
+
+      it("should not fund a wallet with wrong account number", async () => {
+        const res3 = await req
+          .post(`${baseWalletRoute}/fund/${wrongAccountNumber}`)
+          .set("authorization", `Bearer ${userToken}`)
+          .send(completeFundingDetail);
+
+        expect(res3.status).to.equal(404);
+        expect(res3.body).to.be.an("object");
+        expect(res3.body.message).to.equal("Wallet not found");
+        expect(res3.body.status).to.equal("failure");
+      });
     });
   });
 
-  describe("Wallet Funding", () => {
-    let res: ChaiHttp.Response;
-    before(async () => {
-      res = await req
-        .post(`${baseWalletRoute}/create`)
-        .set("authorization", `Bearer ${userToken}`)
-        .send(completeWalletDetail);
-    });
+  describe("Multiple Wallet", () => {
+    describe("Wallet Creation and Funding", () => {
+      let res: ChaiHttp.Response;
 
-    it("should not fund a wallet with empty amount", async () => {
-      const res1 = await req
-        .post(`${baseWalletRoute}/fund/${res.body.data.account_number}}`)
-        .set("authorization", `Bearer ${userToken}`)
-        .send(incorrectFundingDetail1);
+      it("should allow authenticated user to create more than one wallet", async () => {
+        res = await req
+          .post(`${baseWalletRoute}/create-multiple`)
+          .set("authorization", `Bearer ${userToken}`)
+          .send(completeWalletDetail);
 
-      expect(res1.status).to.equal(400);
-      expect(res1.body).to.be.an("object");
-      expect(res1.body.message).to.equal(
-        "Your missed a required fields: amount"
-      );
-      expect(res1.body.status).to.equal("failure");
-    });
+        expect(res.status).to.equal(201);
+        expect(res.body).to.be.an("object");
+        expect(res.body.message).to.equal("Wallet created successfully");
+        expect(res.body.data).to.be.an("object");
+        expect(res.body.data).to.have.property("id");
+        expect(res.body.data).to.have.property("user_id");
+        expect(res.body.data).to.have.property("balance");
+        expect(res.body.data.balance).to.equal("0.00");
+        expect(res.body.data).to.have.property("currency");
+        expect(res.body.data).to.have.property("account_name");
+        expect(res.body.data).to.have.property("account_number");
+      });
 
-    it("should not fund a wallet with invalid amount format", async () => {
-      const res2 = await req
-        .post(`${baseWalletRoute}/fund/${res.body.data.account_number}}`)
-        .set("authorization", `Bearer ${userToken}`)
-        .send(incorrectFundingDetail2);
+      it("should allow user to fund a wallet", async () => {
+        const res2 = await req
+          .post(`${baseWalletRoute}/fund/${res.body.data.account_number}`)
+          .set("authorization", `Bearer ${userToken}`)
+          .send(completeFundingDetail);
 
-      expect(res2.status).to.equal(400);
-      expect(res2.body).to.be.an("object");
-      expect(res2.body.message).to.equal("Invalid amount format");
-      expect(res2.body.status).to.equal("failure");
-    });
-
-    it("should not fund a wallet with wrong account number", async () => {
-      const res3 = await req
-        .post(`${baseWalletRoute}/fund/}`)
-        .set("authorization", `Bearer ${userToken}`)
-        .send(completeFundingDetail);
-
-      expect(res3.status).to.equal(404);
-      expect(res3.body).to.be.an("object");
-      expect(res3.body.message).to.equal("Wallet not found");
-      expect(res3.body.status).to.equal("failure");
-    });
-
-    it("should allow user to fund a wallet", async () => {
-      const res4 = await req
-        .post(`${baseWalletRoute}/fund/${res.body.data.account_number}`)
-        .set("authorization", `Bearer ${userToken}`)
-        .send(completeFundingDetail);
-
-      expect(res4.status).to.equal(200);
-      expect(res4.body).to.be.an("object");
-      expect(res4.body.message).to.equal("Wallet funded successfully");
-      expect(res4.body.data).to.be.an("object");
-      expect(res4.body.data).to.have.property("id");
-      expect(res4.body.data).to.have.property("user_id");
-      expect(res4.body.data).to.have.property("balance");
-      expect(res4.body.data.balance).to.equal("1000.00");
-      expect(res4.body.data).to.have.property("currency");
-      expect(res4.body.data).to.have.property("account_name");
-      expect(res4.body.data).to.have.property("account_number");
+        expect(res2.status).to.equal(200);
+        expect(res2.body).to.be.an("object");
+        expect(res2.body.message).to.equal("Wallet funded successfully");
+        expect(res2.body.data).to.be.an("object");
+        expect(res2.body.data).to.have.property("id");
+        expect(res2.body.data).to.have.property("user_id");
+        expect(res2.body.data).to.have.property("balance");
+        expect(res2.body.data.balance).to.equal("1000.00");
+        expect(res2.body.data).to.have.property("currency");
+        expect(res2.body.data).to.have.property("account_name");
+        expect(res2.body.data).to.have.property("account_number");
+      });
     });
   });
 });
