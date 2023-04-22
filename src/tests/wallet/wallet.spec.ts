bdf274e8bf2d05ad4ab412afbba8fa3d1ad19c51
Johnson Ojo <johnsonojo89@gmail.com>
2023-04-22 12:11:34 +0100

    added uint tests for inter wallet transfer feature

--- src/tests/wallet/wallet.spec.ts
+++ src/tests/wallet/wallet.spec.ts
@@ -12,10 +12,28 @@ const {
   incorrectFundingDetail1,
   incorrectFundingDetail2,
   wrongAccountNumber,
+  completeTransferDetail1,
+  completeTransferDetail2,
+  inSufficientFundDetail,
+  seededWallet1,
+  incorrectFundingNumber,
+  incompleteTransferDetail1,
+  incompleteTransferDetail2,
+  incompleteTransferDetail3,
+  incompleteTransferDetail4,
 } = walletDetails;
-const { seededUser, malformedToken, expiredToken } = userDetails;
 
-let userToken: string;
+const {
+  seededUserWithWallet1,
+  seededUserWithWallet2,
+  malformedToken,
+  expiredToken,
+  seededUserWithoutWallet,
+} = userDetails;
+
+let userOneToken: string;
+let userTwoToken: string;
+let userThreeToken: string;
 let req: ChaiHttp.Agent;
 
 const loginRoute = "/api/v1/auth/login";
@@ -30,8 +48,20 @@ describe("Wallet Controller", () => {
   });
 
   before(async () => {
-    let loggedIn = await req.post(`${loginRoute}`).send(seededUser);
-    userToken = loggedIn.body.data.accessToken;
+    let loggedInUserOne = await req
+      .post(`${loginRoute}`)
+      .send(seededUserWithWallet1);
+    userOneToken = loggedInUserOne.body.data.accessToken;
+
+    let loggedInUserTwo = await req
+      .post(`${loginRoute}`)
+      .send(seededUserWithWallet2);
+    userTwoToken = loggedInUserTwo.body.data.accessToken;
+
+    let loggedInUserThree = await req
+      .post(`${loginRoute}`)
+      .send(seededUserWithoutWallet);
+    userThreeToken = loggedInUserThree.body.data.accessToken;
   });
 
   after(async () => {
@@ -45,7 +75,7 @@ describe("Wallet Controller", () => {
       it("should allow authenticated user to create a wallet", async () => {
         res = await req
           .post(`${baseWalletRoute}/create`)
-          .set("authorization", `Bearer ${userToken}`)
+          .set("authorization", `Bearer ${userThreeToken}`)
           .send(completeWalletDetail);
 
         expect(res.status).to.equal(201);
@@ -61,22 +91,10 @@ describe("Wallet Controller", () => {
         expect(res.body.data).to.have.property("account_number");
       });
 
-      it("should not allow authenticated user to create more than one wallet", async () => {
-        const res2 = await req
-          .post(`${baseWalletRoute}/create`)
-          .set("authorization", `Bearer ${userToken}`)
-          .send(completeWalletDetail);
-
-        expect(res2.status).to.equal(409);
-        expect(res2.body).to.be.an("object");
-        expect(res2.body.message).to.equal("User already has a wallet");
-        expect(res2.body.status).to.equal("failure");
-      });
-
       it("should allow user to fund a wallet", async () => {
         const res2 = await req
           .post(`${baseWalletRoute}/fund/${res.body.data.account_number}`)
-          .set("authorization", `Bearer ${userToken}`)
+          .set("authorization", `Bearer ${userThreeToken}`)
           .send(completeFundingDetail);
 
         expect(res2.status).to.equal(200);
@@ -95,7 +113,7 @@ describe("Wallet Controller", () => {
       it("should not fund a wallet with empty amount", async () => {
         const res1 = await req
           .post(`${baseWalletRoute}/fund/${res.body.data.account_number}}`)
-          .set("authorization", `Bearer ${userToken}`)
+          .set("authorization", `Bearer ${userThreeToken}`)
           .send(incorrectFundingDetail1);
 
         expect(res1.status).to.equal(400);
@@ -109,12 +127,24 @@ describe("Wallet Controller", () => {
       it("should not fund a wallet with invalid amount format", async () => {
         const res2 = await req
           .post(`${baseWalletRoute}/fund/${res.body.data.account_number}}`)
-          .set("authorization", `Bearer ${userToken}`)
+          .set("authorization", `Bearer ${userThreeToken}`)
           .send(incorrectFundingDetail2);
 
         expect(res2.status).to.equal(400);
         expect(res2.body).to.be.an("object");
         expect(res2.body.message).to.equal("Invalid amount format");
+        expect(res2.body.status).to.equal("failure");
+      });
+
+      it("should not allow authenticated user to create more than one wallet", async () => {
+        const res2 = await req
+          .post(`${baseWalletRoute}/create`)
+          .set("authorization", `Bearer ${userThreeToken}`)
+          .send(completeWalletDetail);
+
+        expect(res2.status).to.equal(409);
+        expect(res2.body).to.be.an("object");
+        expect(res2.body.message).to.equal("User already has a wallet");
         expect(res2.body.status).to.equal("failure");
       });
     });
@@ -123,7 +153,7 @@ describe("Wallet Controller", () => {
       it("should not create a wallet with empty currency", async () => {
         const res = await req
           .post(`${baseWalletRoute}/create`)
-          .set("authorization", `Bearer ${userToken}`)
+          .set("authorization", `Bearer ${userTwoToken}`)
           .send(incorrectWalletDetail1);
 
         expect(res.status).to.equal(400);
@@ -137,7 +167,7 @@ describe("Wallet Controller", () => {
       it("should not create a wallet with invalid currency format", async () => {
         const res = await req
           .post(`${baseWalletRoute}/create`)
-          .set("authorization", `Bearer ${userToken}`)
+          .set("authorization", `Bearer ${userTwoToken}`)
           .send(incorrectWalletDetail2);
 
         expect(res.status).to.equal(400);
@@ -185,7 +215,7 @@ describe("Wallet Controller", () => {
       it("should not fund a wallet with wrong account number", async () => {
         const res3 = await req
           .post(`${baseWalletRoute}/fund/${wrongAccountNumber}`)
-          .set("authorization", `Bearer ${userToken}`)
+          .set("authorization", `Bearer ${userOneToken}`)
           .send(completeFundingDetail);
 
         expect(res3.status).to.equal(404);
@@ -203,7 +233,7 @@ describe("Wallet Controller", () => {
       it("should allow authenticated user to create more than one wallet", async () => {
         res = await req
           .post(`${baseWalletRoute}/create-multiple`)
-          .set("authorization", `Bearer ${userToken}`)
+          .set("authorization", `Bearer ${userOneToken}`)
           .send(completeWalletDetail);
 
         expect(res.status).to.equal(201);
@@ -222,7 +252,7 @@ describe("Wallet Controller", () => {
       it("should allow user to fund a wallet", async () => {
         const res2 = await req
           .post(`${baseWalletRoute}/fund/${res.body.data.account_number}`)
-          .set("authorization", `Bearer ${userToken}`)
+          .set("authorization", `Bearer ${userOneToken}`)
           .send(completeFundingDetail);
 
         expect(res2.status).to.equal(200);
@@ -236,6 +266,124 @@ describe("Wallet Controller", () => {
         expect(res2.body.data).to.have.property("currency");
         expect(res2.body.data).to.have.property("account_name");
         expect(res2.body.data).to.have.property("account_number");
+      });
+    });
+  });
+
+  describe("Inter Wallet Transfer", () => {
+    describe("Wallet Transfer", () => {
+      it("should allow funds to be transferred to another user", async () => {
+        const res = await req
+          .post(`${baseWalletRoute}/transfer/${seededWallet1.account_number}`)
+          .set("authorization", `Bearer ${userOneToken}`)
+          .send(completeTransferDetail1);
+
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an("object");
+        expect(res.body.message).to.equal("Wallet funded successfully");
+        expect(res.body.status).to.equal("success");
+      });
+
+      it("should not allow transfer with empty amount field", async () => {
+        const res = await req
+          .post(`${baseWalletRoute}/transfer/${seededWallet1.account_number}`)
+          .set("authorization", `Bearer ${userOneToken}`)
+          .send(incompleteTransferDetail1);
+
+        expect(res.status).to.equal(400);
+        expect(res.body).to.be.an("object");
+        expect(res.body.message).to.equal(
+          "Your missed a required fields: amount"
+        );
+        expect(res.body.status).to.equal("failure");
+      });
+
+      it("should not allow transfer with malformed amount field", async () => {
+        const res = await req
+          .post(`${baseWalletRoute}/transfer/${seededWallet1.account_number}`)
+          .set("authorization", `Bearer ${userOneToken}`)
+          .send(incompleteTransferDetail2);
+
+        expect(res.status).to.equal(400);
+        expect(res.body).to.be.an("object");
+        expect(res.body.message).to.equal("Invalid amount format");
+        expect(res.body.status).to.equal("failure");
+      });
+
+      it("should not allow transfer with empty receiverAccountNumber  field", async () => {
+        const res = await req
+          .post(`${baseWalletRoute}/transfer/${seededWallet1.account_number}`)
+          .set("authorization", `Bearer ${userOneToken}`)
+          .send(incompleteTransferDetail3);
+
+        expect(res.status).to.equal(400);
+        expect(res.body).to.be.an("object");
+        expect(res.body.message).to.equal(
+          "Your missed a required fields: receiverAccountNumber"
+        );
+        expect(res.body.status).to.equal("failure");
+      });
+
+      it("should not allow transfer with empty receiverAccountNumber  field", async () => {
+        const res = await req
+          .post(`${baseWalletRoute}/transfer/${seededWallet1.account_number}`)
+          .set("authorization", `Bearer ${userOneToken}`)
+          .send(incompleteTransferDetail4);
+
+        expect(res.status).to.equal(400);
+        expect(res.body).to.be.an("object");
+        expect(res.body.message).to.equal("Invalid receiver account number");
+        expect(res.body.status).to.equal("failure");
+      });
+
+      it("should not allow transfer to a wallet that does not exist", async () => {
+        const res = await req
+          .post(`${baseWalletRoute}/transfer/${wrongAccountNumber}`)
+          .set("authorization", `Bearer ${userOneToken}`)
+          .send(completeTransferDetail1);
+
+        expect(res.status).to.equal(404);
+        expect(res.body).to.be.an("object");
+        expect(res.body.message).to.equal("Wallet not found");
+        expect(res.body.status).to.equal("failure");
+      });
+
+      it("should not allow transfer with same sender and receiver account", async () => {
+        const res = await req
+          .post(`${baseWalletRoute}/transfer/${seededWallet1.account_number}`)
+          .set("authorization", `Bearer ${userOneToken}`)
+          .send(completeTransferDetail2);
+
+        expect(res.status).to.equal(400);
+        expect(res.body).to.be.an("object");
+        expect(res.body.message).to.equal(
+          "You cannot transfer fund to yourself"
+        );
+        expect(res.body.status).to.equal("failure");
+      });
+
+      it("should not allow transfer with insufficient balance", async () => {
+        const res = await req
+          .post(`${baseWalletRoute}/transfer/${seededWallet1.account_number}`)
+          .set("authorization", `Bearer ${userOneToken}`)
+          .send(inSufficientFundDetail);
+
+        expect(res.status).to.equal(400);
+        expect(res.body).to.be.an("object");
+        expect(res.body.message).to.equal("Insufficient funds");
+        expect(res.body.status).to.equal("failure");
+      });
+
+      it("should not allow transfer to account that doesn't exist", async () => {
+        const res = await req
+          .post(`${baseWalletRoute}/transfer/${seededWallet1.account_number}`)
+          .set("authorization", `Bearer ${userOneToken}`)
+          .send(incorrectFundingNumber);
+
+        expect(res.status).to.equal(404);
+        expect(res.body).to.be.an("object");
+        expect(res.body.message).to.equal("Receiver wallet does not exist");
+        expect(res.body.status).to.equal("failure");
       });
     });
   });
